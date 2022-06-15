657a20d370ca0be723cb997c062f5d0d3cd2c59b
luis-fernando-chambi-orosco <104604119+luis-fernando-chambi-orosco@users.noreply.github.com>
2022-06-14 22:28:39 -0500

    comentarios

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -16,16 +16,19 @@ public class Exercise1 {
     public boolean esBalanceado(String s) {
         // Colocar codigo aqui
              MyStack<Character> stack = new LinkedListStack<>();
-        // Colocar codigo aqui
+        // leemos letra por letra y guardamos los abiertos "("
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
                 stack.push(s.charAt(i));
             }
+            //comparamos el elemento de la parte superior de la pila y verifica que sea del mismo tipo
             if (stack.top() == '(' && s.charAt(i) == ')' || stack.top() == '[' && s.charAt(i) == ']'
                     || stack.top() == '{' && s.charAt(i) == '}') {
+                // eliminamos el ultimo elemento que se agrego
                 stack.pop();
             }
         }
+        // si la pila es vacia true 
         return stack.isEmpty();
     }
 }
