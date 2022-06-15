3e6f3ae163e86a9732542352b8064645ef9235b9
luis-fernando-chambi-orosco <104604119+luis-fernando-chambi-orosco@users.noreply.github.com>
2022-06-14 20:50:48 -0500

    Update MyQueue.java

--- src/main/java/com/example/project/MyQueue.java
+++ src/main/java/com/example/project/MyQueue.java
@@ -2,15 +2,15 @@ package com.example.project;
 
 public interface MyQueue<T extends Comparable<T>> {
 
-    // Métodos que modifican la cola
-    public void enqueue(T v); // Poner un valor al final de la cola
+  
+    public void enqueue(T v); 
 
-    public T dequeue(); // Elimina y devuelve el valor al principio de la cola
+    public T dequeue(); 
 
-    // Métodos que acceden a la información (sin modificar)
-    public T first(); // Valor devuelto al principio de la cola
+ 
+    public T first(); 
 
-    public int size(); // Devuelve el número de elementos en la cola
+    public int size();
 
-    public boolean isEmpty(); // Indica si la cola está vacía
+    public boolean isEmpty();
 }
